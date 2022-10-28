e5e211105e131eabb887af9ac9fcf6cd2af2e74a
Monica Vazquez <monicavazquez478@gmail.com>
2022-10-28 18:18:59 -0300

    Empezando con post delete

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,12 +1,13 @@
+const { response } = require('express')
 const Post = require('../models/posts')
-
+//index
 const getPosts = async (req, res) => {
     
     try {
     
         const posts = await Post.find({}).lean()
        // console.log(posts)
-        const title = "Listado de Posts"        
+        const title = "InfoBlog - Listado de Posts"        
         res.status(200).render('index',
             {
                 title,
@@ -15,11 +16,27 @@ const getPosts = async (req, res) => {
         )
 
     } catch (error) {
-        console.log(error)
+        console.log('Error index', error)
         
+    }
+}
+// show
+const showPost = async (req, res = response) => {
+    try {
+        const post = await Post.findOne({ slug : req.params.slug }).lean()
+        if ( post === null ) return res.redirect('/')
+
+    res.render('show', 
+    {
+        title: `InfoBlog - ${post.title}`,
+        post
+    });
+    } catch (error) {
+        console.log('error show', error)
     }
 }
 
 module.exports = {
-    getPosts
+    getPosts,
+    showPost
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const routerPosts = express.Router()
-const { getPosts } = require('../controllers/posts')
+
+const { getPosts, showPost } = require('../controllers/posts')
 
 // Rutas de INDEX
 routerPosts.get('/posts', getPosts)
+routerPosts.get('/posts/:slug', showPost)
 
 
 module.exports = {
